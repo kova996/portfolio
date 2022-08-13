04cd4e1f7c1c757e01a1cde6c5a1491fe6d962af
kova996 <lukakovacusic@gmail.com>
2022-08-13 10:13:26 +0200

    hehe

---
+++ src/components/global/navigation.js
@@ -0,0 +1,13 @@
+
+
+export function DummyNavigation(props) {
+    return (
+        <nav>
+            <ul>
+                <li><button>Home</button></li>
+                <li><button>...</button></li>
+                <li><button>Contact</button></li>
+            </ul>
+        </nav>
+    )
+}
