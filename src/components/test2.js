b109007ea895a66c86c6b1338d62b9bf5dcab089
kova996 <lukakovacusic@gmail.com>
2022-08-13 09:43:12 +0200

    style object fixed

--- src/components/test2.js
+++ src/components/test2.js
@@ -2,7 +2,9 @@
 
 export function HelloLuka(props) {
     return (
-        <div style="border: 1px solid black;">
+        <div style={{
+                border: "1px solid black"
+            }}>
             <h1>This is my first official page</h1>
             <p>This is my first text that is live</p>
         </div>
