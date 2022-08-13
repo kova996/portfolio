797b1a84f7b45518a8cb5e579b25e5722ab9f4b7
kova996 <lukakovacusic@gmail.com>
2022-08-13 09:33:14 +0200

    first changes after managed redirects

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Test from './components/test';
+import { HelloLuka } from './components/test2';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <HelloLuka></HelloLuka>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/test2.js
@@ -0,0 +1,10 @@
+
+
+export function HelloLuka() {
+    return (
+        <div style="border: 1px solid black;">
+            <h1>This is my first official page</h1>
+            <p>This is my first text that is live</p>
+        </div>
+    )
+}
