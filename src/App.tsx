60da7b59fe573fbec48facf4b1ceb4d1e171d6bb
kova996 <lukakovacusic@gmail.com>
2022-09-26 18:21:09 +0200

    Switched to ReactTS

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,14 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Test from './components/test';
-import { HelloLuka } from './components/test2';
-import { DummyNavigation } from './components/global/navigation';
 
-function App(props) {
+function App() {
   return (
     <div className="App">
-      <DummyNavigation></DummyNavigation>
       <header className="App-header">
-        <HelloLuka></HelloLuka>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -20,9 +16,8 @@ function App(props) {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React every day
+          Learn React
         </a>
-        <Test />
       </header>
     </div>
   );
