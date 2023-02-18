289713f481ccf121531ef0e1766a64f267d849a7
kova996 <lukakovacusic@gmail.com>
2023-02-18 20:14:05 +0100

    Revert "removed unused imports"
    This reverts commit 74ccc7a2169297278d69634bfc32a52e0763472d.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Layout } from './layouts/MainLayout';
