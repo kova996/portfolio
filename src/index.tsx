74ccc7a2169297278d69634bfc32a52e0763472d
kova996 <lukakovacusic@gmail.com>
2023-02-18 19:51:11 +0100

    removed unused imports

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Layout } from './layouts/MainLayout';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
